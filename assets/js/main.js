6c4286390d6d5583ab15dc647c8a3b9bb51b9073
AhmadAfwanFaizi <ahmadafwanfaizi@gmail.com>
2023-06-15 14:54:42 +0700

    scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,13 +158,13 @@ window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
 
-// function scrollTop() {
-//   const scrollTop = document.getElementById("scroll-top");
-//   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-//   if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
-//   else scrollTop.classList.remove("show-scroll");
-// }
-// window.addEventListener("scroll", scrollTop);
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
 
